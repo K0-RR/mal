a0bd5977350d4abf97c7d1a6d209f7bf4cdc2e27
K0RR <43277609+RDKRACZ@users.noreply.github.com>
2023-07-29 09:01:43 +0200

    Update mal.py

--- mal.py
+++ mal.py
@@ -1,6 +1,6 @@
 import requests
 n = 0
-URL = "https://malsignature.com/?/view?username=k0rr&style=normal"
+URL = "https://malsignature.com/?/view?username=fuck-anime&style=normal"
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36",
     "Connection": "keep-alive"
